8f5a67363e93f68adca03d23583564178c2829de
GitHub Action <action@github.com>
2021-04-13 08:46:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/felixgeyer/scintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.felixgeyer.scintellijplugin.services
+
+import com.github.felixgeyer.scintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/felixgeyer/scintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/felixgeyer/scintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.felixgeyer.scintellijplugin.services
 
+import com.github.felixgeyer.scintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
